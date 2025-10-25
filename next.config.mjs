6defc764356107d3cbc695a6a81d745257f6940a
findloveinpattaya-ship-it <findlovein.pattaya@gmail.com>
2025-10-26 00:11:03 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  experimental: { serverActions: true },
+  output: 'standalone',
+};
 export default nextConfig;
